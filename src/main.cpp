3534bea8f81efb7e1eab40c582eff614ca37eb2b
DavidHemeryck <david.hemeryck@outlook.com>
2021-02-24 10:13:17 +0100

    Update main.cpp
    Multi-Wifi werkt (soort van)

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,53 @@
 #include <WiFi.h>
-#include <Arduino.h>
-#include <circ-buffer.h>
- 
-const char* ssid = "NETGEAR68";
-const char* password =  "excitedtuba713";
- 
-CircBuffer buffer;
-int teller=0;
-float * avg;
+#include <WiFiMulti.h>
 
-CircBufferStatus_t initBuffers(uint8_t size){
-  CircBufferStatus_t status = buffer.init(size);
-  if(status != CB_SUCCESS){
-  return status;
-}
-  return CB_SUCCESS;
+WiFiMulti wifiMulti;
+int teller=1;
+unsigned long tijd1=0;
+unsigned long tijd2=0;
+unsigned long tijd3=0;
+unsigned long tijd4=0;
+unsigned long tijd=0;
+
+void setup(){
+    Serial.begin(115200);
+    wifiMulti.addAP("ESP32-Access-Point-1", "123456789");
+    wifiMulti.addAP("ESP32-Access-Point-2", "987654321");
+    Serial.println("Connecting Wifi...");
+    if(wifiMulti.run() == WL_CONNECTED) {
+      Serial.print("WiFi connected: ");
+      Serial.println(WiFi.SSID());
+      Serial.println(WiFi.localIP());
+    }    
 }
 
-void connectToNetwork() {
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Establishing connection to WiFi..");
-  }
- 
-  Serial.println("Connected to network");
- 
-}
- 
-void setup() {
- 
-  Serial.begin(115200);
-  pinMode(2,OUTPUT);
-  connectToNetwork();
-  initBuffers(10);
-  Serial.println(WiFi.macAddress());
-  Serial.println(WiFi.localIP());
- 
-
- 
-}
- 
 void loop() {
-  delay(100);
-  long rssi = WiFi.RSSI();
-  buffer.put(rssi);
+  // Maintain WiFi connection
+  Serial.print("RSSI: ");
+  Serial.println(WiFi.RSSI());
+  Serial.println(teller);
+  if(teller==0){
+    tijd3=millis();
+    Serial.println("Hier");
+  }
   teller++;
-  if(teller%10==0){
-    Serial.printf("Gemiddelde: %f\n", buffer.getAverage());
-    if(buffer.getAverage()>-40){
-      digitalWrite(2,HIGH);
+  if(teller==100){
+    tijd4=millis();
+    teller=0;
+    tijd = tijd4-tijd3;
+    Serial.println("Tijd teller=100: ");
+    Serial.println(tijd);
+    tijd1 = millis();
+    if (wifiMulti.run() == WL_CONNECTED) {
+      Serial.print("WiFi connected: ");
+      Serial.println(WiFi.SSID());  
+      Serial.println(WiFi.localIP());
+    } else {
+      Serial.println("WiFi not connected!");
     }
-    else{
-      if(buffer.getAverage()<-45){
-        digitalWrite(2,LOW);
-      }
-    }
+    tijd2 = millis();
+    tijd=tijd2-tijd1; 
+    Serial.println("Tijd connecteren");
+    Serial.println(tijd);
   }
-  if(teller%20==0){
-    WiFi.disconnect(true);
-    if(strcmp(ssid,ssid1) == 0){
-      ssid=ssid2;
-      password=password2;
-    }else{
-      ssid=ssid1;
-      password=password1;
-    }
-    connectToNetwork();
-  }
-
 }
